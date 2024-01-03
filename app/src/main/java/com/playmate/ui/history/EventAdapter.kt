117c13ef29352377e1fbefdf52f0f5f22cc67f0d
max <max.de.la.gastine@hotmail.com>
2024-01-03 17:00:59 +0100

    La notation est plus intuitive

--- app/src/main/java/com/playmate/ui/history/EventAdapter.kt
+++ app/src/main/java/com/playmate/ui/history/EventAdapter.kt
@@ -1,6 +1,7 @@
 package com.playmate.ui.history
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.playmate.DataBase
@@ -29,7 +31,12 @@ class EventAdapter(private val eventList: List<EventList>, private val ratingCha
         val textEventRequiredLevel: TextView = itemView.findViewById(R.id.text_event_required_level)
         val textEventParticipating: TextView = itemView.findViewById(R.id.text_event_participating)
         val textEventAddress: TextView = itemView.findViewById(R.id.text_event_address)
+        val buttonRateEvent: Button = itemView.findViewById(R.id.buttonRateEvent)
         val seekBarRating: SeekBar = itemView.findViewById(R.id.seek_bar_rating)
+        val buttonSubmitRating: Button = itemView.findViewById(R.id.buttonSubmitRating)
+        val textRating: TextView = itemView.findViewById(R.id.text_rating)
+
+
 
     }
 
@@ -52,7 +59,7 @@ class EventAdapter(private val eventList: List<EventList>, private val ratingCha
 
         holder.textEventSport.text = "Votre sport : ${event.sport}"
         holder.textEventDate.text = "La seance a eu lieu le : ${event.date}"
-        holder.textEventTime.text = "Votre horaire etait : ${event.time}"
+        holder.textEventTime.text = "Votre horaire etait a : ${event.time}"
         holder.textEventDuration.text = "Elle a duree : ${event.duration} h"
         holder.textEventMaxPeople.text = "Il y avait maximum : ${event.maxPeople} personne(s)"
         holder.textEventRequiredEquipement.text = "L'equipement necessaire etait : ${event.requiredEquipment}"
@@ -60,30 +67,41 @@ class EventAdapter(private val eventList: List<EventList>, private val ratingCha
         holder.textEventParticipating.text = "Vous etiez : ${event.participating} participant"
         holder.textEventAddress.text = "La seance etait a l'addresse suivante : ${event.address}"
 
-
-
-        // Set click listener on header to show/hide details
         holder.eventTitle.setOnClickListener {
             toggleEventDetails(holder.eventDetails)
         }
 
+        holder.buttonRateEvent.setOnClickListener {
+            // Demander à l'utilisateur s'il souhaite noter la séance
+            showRatingDialog(holder)
+        }
+
+        // Dans onBindViewHolder
         holder.seekBarRating.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                val rating = progress + 1
-                ratingChangeListener.onRatingChanged(rating)
+
+                val ratingValue = progress
+                holder.textRating.text = "$ratingValue sur 5"
+                holder.buttonSubmitRating.visibility = View.VISIBLE
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
+                // Ce qui se passe lorsque l'utilisateur commence à déplacer le curseur
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
+                // Ce qui se passe lorsque l'utilisateur a terminé de déplacer le curseur
             }
-
-            // Reste du code
         })
 
+
+        holder.buttonSubmitRating.setOnClickListener {
+            val rating = holder.seekBarRating.progress
+            ratingChangeListener.onRatingChanged(rating)
+            val message = "Note envoyée : $rating"
+            // Afficher le message dans un Toast ou un autre composant d'affichage
+            Toast.makeText(holder.itemView.context, message, Toast.LENGTH_SHORT).show()
+        }
 
     }
 
@@ -99,4 +117,25 @@ class EventAdapter(private val eventList: List<EventList>, private val ratingCha
             layout.visibility = View.VISIBLE
         }
     }
+
+    private fun showRatingDialog(holder: EventViewHolder) {
+        val ratingDialog = AlertDialog.Builder(holder.itemView.context)
+            .setTitle("Noter la séance")
+            .setMessage("Voulez-vous noter cette séance ?")
+            .setPositiveButton("Oui") { dialog, _ ->
+                // Afficher la SeekBar
+                holder.seekBarRating.visibility = View.VISIBLE
+                // Masquer le bouton
+                holder.buttonRateEvent.visibility = View.GONE
+                dialog.dismiss()
+            }
+            .setNegativeButton("Non") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+
+        ratingDialog.show()
+    }
+
+
 }
